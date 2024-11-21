59e0b3e250967db510c356f421021e42b1fb32e5
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-11-21 16:43:52 -0300

    21/11

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,6 +1,9 @@
 const url = 'https://raw.githubusercontent.com/TamirisCeolin/ciencia-de-dados-3C/refs/heads/main/dados-globais.json';
 
 async function visualizarInformacoesGlobais(){
+    const res = await fetch(url);
+
+    const dados = await res.json();
 
 }
 
